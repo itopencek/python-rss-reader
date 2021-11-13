8610327348d2267c786c9a26d89c214c39f2ced1
Ivan Havasi <ivohavasi@gmail.com>
2021-11-13 18:43:18 +0100

    Added Flask

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,13 +1,17 @@
 from src.parser.rssparser import RssParser
 from src.reader.webreader import WebReader
+from flask import Flask
+app = Flask(__name__)
 
 
+@app.route('/')
 def main():
     parser = RssParser()
     reader = WebReader()
     website = reader.read("https://www.sme.sk/rss-title")
-    print(parser.parse(website).values)
+    data = parser.parse(website).values
+    return str(data)
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
