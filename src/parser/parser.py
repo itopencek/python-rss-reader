605baecc7c0c8156e335834dad4882d832c2a487
Ivan Havasi <ivohavasi@gmail.com>
2021-11-13 18:30:04 +0100

    Added basic parser

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,9 +1,9 @@
 class Parser:
-    def parse(self, input):
+    def parse(self, data):
         """
         Abstract method, which parses data.
 
-        :param input: data, which needs to be parsed
+        :param data: data, which needs to be parsed
         :return: parsed data
         """
         pass
--- src/parser/rssparser.py
+++ src/parser/rssparser.py
@@ -1,38 +1,50 @@
 import pandas
+import xml.etree.ElementTree as ET
 from src.parser.parser import Parser
 
 
 class RssParser(Parser):
-    id = 0
+    next_id = 0
 
     def __init__(self):
         pass
 
-    def __get_data_frame(self):
+    @staticmethod
+    def __get_data_frame():
         """
         Creates pandas data frame.
+
         :return: Pandas data frame
         """
-        return pandas.DataFrame(columns=['id', 'title', 'published', 'description', 'url'])
+        return pandas.DataFrame(columns=['id', 'title', 'published', 'description', 'url', 'source'])
 
-    def parse(self, input):
+    def parse(self, data):
         """
         Parses RSS input into Pandas data_frame.
 
-        :param input: RSS input
+        :param data: RSS input - xml in string
         :return: Pandas data_frame
         """
         data_frame = self.__get_data_frame()
-        items = input.html.find("item", first=False)
-
-        for item in items:
-
-            title = item.find('title', first=True).text
-            pubDate = item.find('pubDate', first=True).text
-            guid = item.find('guid', first=True).text
-            description = item.find('description', first=True).text
-
-            row = {'title': title, 'pubDate': pubDate, 'guid': guid, 'description': description}
+        root = ET.fromstring(data)
+        channel = root.find("channel")
+        source = channel.find("link").text
+        for item in channel.findall("item"):
+            title = item.find('title').text
+            published = item.find('pubDate').text
+            description = item.find('description').text
+            url = item.find('link').text
+            row = {'id': self.__next_id(), 'title': title, 'description': description, 'published': published,
+                   'url': url, 'source': source}
             data_frame = data_frame.append(row, ignore_index=True)
 
         return data_frame
+
+    def __next_id(self):
+        """
+        :return: next free id
+        """
+        to_return = self.next_id
+        self.next_id += 1
+        return to_return
+
